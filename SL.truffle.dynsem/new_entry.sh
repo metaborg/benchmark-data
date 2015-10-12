cedb7a7190e81fe733d64a972a355ef388d3c8c7
Vlad Vergu <v.vergu@gmail.com>
2015-10-12 17:50:10 +0300

    Also automatically gather truffle version

--- SL.truffle.dynsem/new_entry.sh
+++ SL.truffle.dynsem/new_entry.sh
@@ -5,5 +5,6 @@ set -e
 TRUFFLESLREV=`git --git-dir=/Users/vladvergu/tud/slde/projects/semantics/truffle-sl/.git rev-parse --short HEAD`
 METABORGSLREV=`git --git-dir=/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/.git rev-parse --short HEAD`
 DSREV=`git --git-dir=/Users/vladvergu/tud/slde/projects/semantics/dynsem/.git rev-parse --short HEAD`
+GRAALREV=`hg -R ~/tud/slde/software/truffle/graal-root/graal-compiler/ id -i`
 
-echo "$TRUFFLESLREV,$METABORGSLREV,$DSREV," >> index.csv
+echo "$TRUFFLESLREV,$METABORGSLREV,$DSREV,$GRAALREV," >> index.csv
