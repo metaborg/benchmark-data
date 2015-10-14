88c29d47d090f5fb4b39014553bf93c31029bd66
Vlad Vergu <v.vergu@gmail.com>
2015-10-14 15:07:20 +0300

    Fine tuning R scripts

--- SL.truffle.dynsem/measurements.r
+++ SL.truffle.dynsem/measurements.r
@@ -7,17 +7,33 @@ runbenchmark <- function(benchmark, variant, variantrev, variantpath) {
   timestamp.datepart <- format(time, "%Y%m%d")
   timestamp.timepart <- format(time, "%H%M%s")
 
-  datafile.graal <- paste("data/data_graal_", timestamp.datepart, "_", timestamp.timepart, ".csv", sep="")
-  datafile.jdk <- paste("data/data_jdk_", timestamp.datepart, "_", timestamp.timepart, ".csv", sep="")
+  datafile.graal <- paste(getwd(), "/data/data_graal_", timestamp.datepart, "_", timestamp.timepart, ".csv", sep="")
+  datafile.jdk <- paste(getwd(), "/data/data_jdk_", timestamp.datepart, "_", timestamp.timepart, ".csv", sep="")
 
-  # runres <- system2("mvn",args=c("exec:exec", "-Dinputfile=",paste(benchmark[2]), "-Dgraaldatafile=", datafile.graal, "-Djdkdatafile=",datafile.jdk))
+  inputarg <- paste("\"", benchmark[2],"/", benchmark[1], "\"", sep="")
+  graaloutarg <- paste("\"", datafile.graal, "\"", sep="")
+  jdkoutarg <- paste("\"", datafile.jdk, "\"", sep="")
 
+  runres <- system2("./runner.sh", args=c(paste(variantpath), inputarg, graaloutarg, jdkoutarg))
+
+  if(runres != 0){
+    print(paste("Error running",benchmark[1],"on language", variant))
+    quit("no", status=runres)
+  }
   newrow <- data.frame(variant, variantrev, dynsem.rev, graal.rev, paste(benchmark[1]), datafile.graal, datafile.jdk)
   write.table(newrow, file=measurements.file, quote=FALSE, append=T, row.names=F, col.names=F,  sep=",")
+
+
+  # Fibonacci-iter.sl,/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/org.metaborg.lang.sl.interp/src/test/resources
+  # CallHeavy.sl,/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/org.metaborg.lang.sl.interp/src/test/resources
+  # Vars.sl,/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/org.metaborg.lang.sl.interp/src/test/resources
+  # BinaryTrees.sl,/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/org.metaborg.lang.sl.interp/src/test/resources
+  # While.sl,/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/org.metaborg.lang.sl.interp/src/test/resources
+
 }
 
 # run Oracle benchmarks
-measurements <- apply(benchmarks, 1, runbenchmark, "Oracle", sl.oracle.rev, sl.oracle.path)
+apply(benchmarks, 1, runbenchmark, "Oracle", sl.oracle.rev, sl.oracle.path)
 
 # run DS benchmarks
 #apply(benchmarks, 1, runbenchmark, "DynSem", sl.metaborg.rev)
---
+++ SL.truffle.dynsem/runner.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+VARIANTPATH="$1"
+INPUTARG="$2"
+GRAALOUT="$3"
+JDKOUT="$4"
+
+CURDIR=`pwd`
+
+cd $VARIANTPATH
+
+mvn exec:exec -Dinputfile=$INPUTARG -Dgraaldatafile=$GRAALOUT -Djdkdatafile=$JDKOUT
+
+echo $INPUTARG $GRAALOUT $JDKOUT
+
+cd $CURDIR
