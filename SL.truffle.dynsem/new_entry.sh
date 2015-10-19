11f4d331817a7f6dd4688388273c66577ad0c6d2
Vlad Vergu <v.vergu@gmail.com>
2015-10-19 15:30:27 +0200

    delete obsolete shell script

--- SL.truffle.dynsem/new_entry.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-METABORGSLREV=`git --git-dir=/Users/vladvergu/tud/slde/projects/semantics/metaborg-sl/.git rev-parse --short HEAD`
-DSREV=`git --git-dir=/Users/vladvergu/tud/slde/projects/semantics/dynsem/.git rev-parse --short HEAD`
-GRAALREV=`hg -R ~/tud/slde/software/truffle/graal-root/graal-compiler/ id -i`
-
-echo "$METABORGSLREV,$DSREV,$GRAALREV," >> index.csv
